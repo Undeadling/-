cf178324d61c656f4331942fac275822fd0c756e
Undeadling <166484824+Undeadling@users.noreply.github.com>
2024-04-09 12:30:56 +0300

    Add files via upload

---
+++ Строки и индексация строк ПЗ.py
@@ -0,0 +1,10 @@
+name = "Это Моя Строка"
+name1 = "Это Новая Строка "
+print(name[0])
+print(name[-1])
+print(name[2:4])
+print(name[4:8])
+print(name[::-1])
+print(len(name))
+print("Это Новая Строка " + name)
+print(name1 + name)
